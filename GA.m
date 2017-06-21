435bb3c789cf0cabe72f7f1323dc773b289812c8
Maty-z <yomatias_199@hotmail.com>
2017-06-21 18:56:17 -0300

    Actualizacion algoritmo genetico aun faltan cosas, ver script

--- GA.m
+++ GA.m
@@ -1,8 +1,31 @@
+% Incompletoooo
+%Falta definir la parte de Select with Replacement!
+
 
 popsize = 20;
 n = 3;
+Best = [];
+iter = 1;
+MaxNumIter: 1000
 
 P = zeros(popsize,n);
 for i = 1:popsize
     P(i,:) = rand(1,n);
+
+while iter < MaxNumIter
+    for i = 1:popsize
+        if f(P(i,:)) < f(Best)
+            Best = P(i,:);
+        end
+    end
+    Q = [];
+    for i = 1:popsize/2
+        Pa = ...
+        Pb = ...
+        [Ca Cb] = cover2(Pa,Pb);
+        Q = [Q mutar(Ca) mutar(Cb)];
+    end
+    P = Q;
+end
+
     
